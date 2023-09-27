14d7bdcce45277d5d2eb3de6fad97d97ddce7bf3
francisz3 <s3971510@student.rmit.edu.au>
2023-09-27 15:40:50 +1000

    super for instructor

--- Instructor.java
+++ Instructor.java
@@ -1,3 +1,8 @@
-class Instructor {
-    super.Employee();
+class Instructor extends Employee {
+
+    public Instructor(String employeeId, String email, String password, String firstName, String lastName,
+            String address, String phoneNumber) {
+        // PUT SHARED ATTRIBUTES OF EMPLOYEE IN SUPER
+        super(employeeId, email, password, firstName, lastName, address, phoneNumber);
+    }
 }
