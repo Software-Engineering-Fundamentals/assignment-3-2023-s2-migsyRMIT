0ead7b247c3883b8b03594c4a982c43e4b20b5dd
migsyRMIT <s3969740@student.rmit.edu.au>
2023-09-27 16:38:48 +1000

    yeet

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Main {    
     public static void main(String[] args) {
-        // Create inquiry
+        // Create instructor object
+        Instructor instructor = new Instructor("12345", "instructor@SEF.com", "password", "John", "Appleseed", "1 Second Street", "0412 345 678");
+
+        // Create inquiry arraylist
+        ArrayList<Integer> InquiryList = new ArrayList<Integer>();
+
+        // Generate inquiry made by the instructor
+        int inquiryID = InquiryList.size() + 1;
+        Date dateMade = new Date();
+        String sender = instructor.getFirstName(); + " " + instructor.getLastName();
+        String status = "pending";
+        String question = "What is the company's name?";
+
+        InquiryList.add(inquiryID);
     }
 }
