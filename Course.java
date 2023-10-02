5664c63fdf2852cfbac6f6d811d3f43f927a3dc3
francisz3 <s3971510@student.rmit.edu.au>
2023-10-02 12:52:44 +1100

    added course registration, course + invoice class

---
+++ Course.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+    private String category;
+    private int lengthWeeks;
+    private Date censusDate;
+    private Instructor coordinater;
+    private ArrayList<Instructor> courseInstructorList;
+    private boolean courseStatus;
+
+    public Course(String courseId, String courseName, String category, int lengthWeeks,
+            Date censusDate, Instructor coordinater, ArrayList<Instructor> courseInstructorList, boolean courseStatus) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.category = category;
+        this.lengthWeeks = lengthWeeks;
+        this.censusDate = censusDate;
+        this.coordinater = coordinater;
+        this.courseInstructorList = courseInstructorList;
+        this.courseStatus = courseStatus;
+    }
+
+    public boolean updateCourseStatus(boolean paymentStatus) {
+        return paymentStatus;
+    }
+
+    public boolean getCourseStatus() {
+        return this.courseStatus;
+    }
+}
